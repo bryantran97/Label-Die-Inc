ee93dbdbd482fffad26183693cc9bbcdadf88bb3
bryantran97@gmail.com <bryantran97@gmail.com>
2017-08-18 18:16:23 -0500

    finishing touches

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+        $(document).ready(function(){
+            $('#wrapper').fadeIn(1300);
+        });
+        $(".about-button").click(function (){
+            $(".index-page").fadeOut('slow', function(){
+               $(".about-page").fadeIn('slow'); 
+            });
+        });
+        
+        $(".return-homepage-button-about").click(function (){
+            $(".about-page").fadeOut('slow', function(){
+                $(".index-page").fadeIn('slow');
+            });
+        });
+        
+        $(".portfolio-button").click(function(){
+            $(".index-page").fadeOut('slow', function() {
+                $(".portfolio-page").fadeIn('slow');
+            });
+        });
+        
+        $(".return-homepage-button-portfolio").click(function (){
+            $(".portfolio-page").fadeOut('slow', function(){
+                $(".index-page").fadeIn('slow');
+            });
+        });
+        $(".resume-button").click(function(){
+            alert("No resume yet!");
+        });
